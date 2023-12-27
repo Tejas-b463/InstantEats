4eb1941f1d0c82f362cdf1063ce45f15d950d0b1
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2023-12-27 17:41:13 +0530

    Laying the Foundation

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World for React!");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(heading);
+const HeadingComponent = () => {
+    return(
+        <h1>Instant Eats</h1>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeadingComponent/>);
