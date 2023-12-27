af1c49833603b08e47fd5a2fcf91220c4d12b0b1
Tejas-b463 <108131310+Tejas-b463@users.noreply.github.com>
2023-12-27 16:43:16 +0530

    Installing package

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1", { id: "heading" }, "Hello World for React!");
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ README.md
@@ -0,0 +1,1 @@
+# InstantEast 🍕
